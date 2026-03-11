35fed4a515289da3536edcf9c7450b5f4200e531
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2026-03-11 15:50:57 +0800

    Delete myproject directory

--- myproject/myhello/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def hello_cgu_view(request):
-    return HttpResponse("Hello CGU")
--- myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('myhello/', include('myhello.urls')), # 把 /myhello/ 交給 myhello 處理
-]
